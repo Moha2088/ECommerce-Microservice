b7bd71e40ab10b66e4d79b42590d00d495ca6249
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2025-03-23 00:07:55 +0100

    add getproducts feature and martendb integration

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record CreateProductResponse(Guid Id);
+
+
+
+public class CreateProductEndpoint : ICarterModule
+{
+    private readonly ISender _sender;
+
+    public CreateProductEndpoint(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var result = await sender.Send(command);
+            var response = result.Adapt<CreateProductResponse>();
+            return Results.Created($"/products/{response.Id}", response);
+        })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Creates a product");
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,26 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Services to the container
 
+builder.Services.AddMarten(opt =>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("DBConnection") ?? throw new InvalidOperationException("Connection string not found!"));
+}).UseLightweightSessions();
+
+builder.Services.AddCarter();
+builder.Services.AddMediatR(opt =>
+{
+    opt.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+
+app.UseHttpsRedirection();
+
+app.MapCarter();
 
 app.Run();
