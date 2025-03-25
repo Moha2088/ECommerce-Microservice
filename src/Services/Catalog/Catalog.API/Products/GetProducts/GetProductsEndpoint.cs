08bc5c5aa316eff6dc6da5a902e1251bd4ec2149
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2025-03-25 03:59:01 +0100

    add pagination

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,5 +1,6 @@
-﻿
-namespace Catalog.API.Products.GetProducts;
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsRequest(int? PageNumber = 1, int? PageSize = 10);
 
 public record GetProductsResponse(IEnumerable<Product> Products);
 
@@ -7,16 +8,17 @@ public class GetProductsEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/products", async (ISender sender) =>
-        {
-            var result = await sender.Send(new GetProductsQuery());
-            var response = result.Adapt<GetProductsResponse>();
-            return Results.Ok(response);
-        })
-        .WithName("GetProducts")
-        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
-        .ProducesProblem(StatusCodes.Status400BadRequest)
-        .WithDescription("Gets a list of products")
-        .WithSummary("Gets a list of products");
+        app.MapGet("/products", async ([AsParameters] GetProductsRequest request, ISender sender) =>
+            {
+                var query = request.Adapt<GetProductsQuery>();
+                var result = await sender.Send(query);
+                var response = result.Adapt<GetProductsResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithDescription("Gets a list of products")
+            .WithSummary("Gets a list of products");
     }
 }
