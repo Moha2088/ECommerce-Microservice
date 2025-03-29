2594d6e02739de1d9c59a950ce75377a42154a48
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2025-03-29 14:42:16 +0100

    add value objects

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<OrderItemId>
+    {
+        public OrderId OrderId { get; set; } = default!;
+        public ProductId ProductId { get; set; } = default!;
+        public int Quantity { get; set; } = default!;
+        public decimal Price { get; set; } = default!;
+
+        internal OrderItem(OrderId orderId, ProductId productId, int quantity, decimal price)
+        {
+            Id = OrderItem.Of(Guid.NewGuid());
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
