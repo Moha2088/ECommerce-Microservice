57288efdca3e9c4f80e5f6c0ab77ba24936a43fe
Moha2088 <113173091+Moha2088@users.noreply.github.com>
2025-04-01 17:47:32 +0200

    add shared messaging library with MassTransit and RabbitMQ configuration

---
+++ src/BuildingBlocks/BuildingBlocks.Messaging/MassTransit/RabbitMQExtensions.cs
@@ -0,0 +1,36 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.MassTransit;
+
+public static class RabbitMQExtensions
+{
+    public static IServiceCollection AddRabbitMQ(this IServiceCollection services,
+         IConfiguration con, Assembly? assembly = null)
+    {
+        services.AddMassTransit(cfg =>
+        {
+            cfg.SetKebabCaseEndpointNameFormatter();
+
+            if (assembly != null)
+            {
+                cfg.AddConsumers(assembly);
+            }
+
+            cfg.UsingRabbitMq((context, configurator) =>
+            {
+                configurator.Host(new Uri(con["MessageBroker:Host"]!), host =>
+                {
+                    host.Username(con["MessageBroker:UserName"]!);
+                    host.Password(con["MessageBroker:Password"]!);
+                });
+
+                configurator.ConfigureEndpoints(context);
+            });
+        });
+
+        return services;
+    }
+}
